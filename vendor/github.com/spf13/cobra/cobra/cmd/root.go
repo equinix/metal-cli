554fa9dda057622fac922ad6c923bc5c44193a67
jasminSPC <jasmin@stackpointcloud.com>
2018-07-11 13:22:11 +0200

    packet project get - fix for number of projects shown

--- vendor/github.com/spf13/cobra/cobra/cmd/root.go
+++ vendor/github.com/spf13/cobra/cobra/cmd/root.go
@@ -43,12 +43,12 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "Jasmin Gacic", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
 	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
 	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
 	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
 	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "JASMIN GACIC <jasmin@stackpointcloud.com>")
+	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
 	viper.SetDefault("license", "apache")
 
 	rootCmd.AddCommand(addCmd)
