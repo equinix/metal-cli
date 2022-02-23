355ad5639bc349f189a65d77ec81cdb1ae20bd84
DailyAlice <msarabia@equinix.com>
2022-02-23 09:25:16 -0600

    editing some wording for man readability for project create

--- internal/projects/create.go
+++ internal/projects/create.go
@@ -41,7 +41,7 @@ func (c *Client) Create() *cobra.Command {
 		Example: `  # Creates a new project named dev-cluster02: 
   metal project create --name dev-cluster02
   
-  # Creates a new project named dev-cluster03 in organization 814b09ca-0d0c-4656-9de0-4ce65c6faf70 with payment method ab1fbdaa-8b25-4c3e-8360-e283852e3747:
+  # Creates a new project named dev-cluster03 in the specified organization with a payment method:
   metal project create -n dev-cluster03 -O 814b09ca-0d0c-4656-9de0-4ce65c6faf70 -m ab1fbdaa-8b25-4c3e-8360-e283852e3747`,
 
 		RunE: func(cmd *cobra.Command, args []string) error {
