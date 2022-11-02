689530471c05aefaa511ffc8f1957dfb9e96d67e
DailyAlice <msarabia@equinix.com>
2022-11-02 10:30:37 -0500

    switching long and short fields to improve help command experience for the operating systems command

--- internal/os/os.go
+++ internal/os/os.go
@@ -36,8 +36,8 @@ func (c *Client) NewCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     `operating-systems`,
 		Aliases: []string{"os"},
-		Short:   "Operating system operations. For more information on which operating systems Equinix Metal offers, visit https://metal.equinix.com/developers/docs/operating-systems/supported/.",
-		Long:    "Operating system operations: get.",
+		Short:   "Operating system operations: get.",
+		Long:    "Information on available operating systems. For more information on which operating systems Equinix Metal offers, visit https://metal.equinix.com/developers/docs/operating-systems/supported/.",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			if root := cmd.Root(); root != nil {
 				if root.PersistentPreRun != nil {
