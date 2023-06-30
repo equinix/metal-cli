adfaf4615cbbc75447b572ad8fdde38652eeeef3
vasubabu <kandimalla.vasubabu@gmail.com>
2023-06-30 21:45:03 +0530

    Added Unit test casse for os sub commands

--- cmd/cli_test.go
+++ cmd/cli_test.go
@@ -4,14 +4,19 @@ import (
 	"testing"
 
 	root "github.com/equinix/metal-cli/internal/cli"
+	"github.com/equinix/metal-cli/internal/os"
 	outputPkg "github.com/equinix/metal-cli/internal/outputs"
 	"github.com/spf13/cobra"
 )
 
 func TestCli_RegisterCommands(t *testing.T) {
+	rootClient := root.NewClient(consumerToken, apiURL, Version)
+	rootCmd := rootClient.NewCommand()
+	rootCmd.DisableSuggestions = false
 	type fields struct {
 		MainCmd  *cobra.Command
 		Outputer outputPkg.Outputer
+		cli *Cli
 	}
 	type args struct {
 		client *root.Client
@@ -31,7 +36,17 @@ func TestCli_RegisterCommands(t *testing.T) {
 				client: &root.Client{},
 			},
 		},
-	}
+		{
+			name: "os",
+			fields: fields{
+				MainCmd: os.NewClient(rootClient, Outputer).NewCommand(),
+				Outputer: outputPkg.Outputer(&outputPkg.Standard{}),
+			},
+			args: args{
+				client: rootClient,
+			},
+		},
+	},
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cli := &Cli{
