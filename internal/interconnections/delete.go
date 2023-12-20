82f0416b87197247364481a905adca51fd8d0541
Ayush Rangwala <ayush.rangwala@gmail.com>
2023-12-20 23:01:27 +0530

    enhance: use force and confirmation while delete interconnections

--- internal/interconnections/delete.go
+++ internal/interconnections/delete.go
@@ -3,22 +3,43 @@ package interconnections
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func (c *Client) Delete() *cobra.Command {
-	var connectionID string
+	var connectionID, confirmation string
+	var force bool
 
 	deleteConnectionCmd := &cobra.Command{
 		Use:   `delete -i <connection_id> `,
 		Short: "Deletes a interconnection.",
-		Long:  "Deletes the specified interconnection.",
+		Long:  "Deletes the specified interconnection. Use --force to skip confirmation",
 		Example: `  # Deletes the specified interconnection:
-  metal interconnections delete -i 7ec86e23-8dcf-48ed-bd9b-c25c20958277`,
+  metal interconnections delete -i 7ec86e23-8dcf-48ed-bd9b-c25c20958277
+  >
+  ✔ Are you sure you want to delete device 7ec86e23-8dcf-48ed-bd9b-c25c20958277 [Y/n]: Y
+`,
 
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cmd.SilenceUsage = true
+
+			if !force {
+				fmt.Printf("Are you sure you want to delete device %s [Y/n]: ", connectionID)
+
+				_, err := fmt.Scanln(&confirmation)
+				if err != nil {
+					fmt.Println("Error reading confirmation:", err)
+					return nil
+				}
+
+				confirmation = strings.TrimSpace(strings.ToLower(confirmation))
+				if confirmation != "yes" && confirmation != "y" {
+					fmt.Println("Interconnection deletion cancelled.")
+					return nil
+				}
+			}
 
 			_, _, err := c.Service.DeleteInterconnection(context.Background(), connectionID).Execute()
 			if err != nil {
@@ -31,6 +52,7 @@ func (c *Client) Delete() *cobra.Command {
 	}
 
 	deleteConnectionCmd.Flags().StringVarP(&connectionID, "id", "i", "", "The UUID of the interconnection.")
+	deleteConnectionCmd.Flags().BoolVarP(&force, "force", "f", false, "Skips confirmation for the interconnection deletion.")
 	_ = deleteConnectionCmd.MarkFlagRequired("id")
 
 	return deleteConnectionCmd
